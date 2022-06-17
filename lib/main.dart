d8cc2ae0d542c9689a91ecb361f56ccd02dcd025
Ken Minami <kenminami.dev@gmail.com>
2022-06-17 20:55:35 +0900

    add flutter_staggered_grid_view

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Fashion Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          elevation: 0,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey[400]
+        )
       ),
       home: HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,8 +9,186 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int _activeCategory = 1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Row(
+              children: [
+                Icon(Icons.bookmark_border, color: Colors.grey),
+                const SizedBox(width: 20.0),
+                Icon(Icons.notifications_none, color: Colors.grey),
+              ],
+            )
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'My page'),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('headline text', style: TextStyle(fontSize: 23.0, fontWeight: FontWeight.w700, color: Colors.black)),
+              const SizedBox(height: 10.0),
+              Text('sub text is here.', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500, color: Colors.grey[600])),
+              const SizedBox(height: 30.0),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    _makeCategoryContainer('For you', 1),
+                    _makeCategoryContainer('Popular', 2),
+                    _makeCategoryContainer('title3', 3),
+                    _makeCategoryContainer('For you', 4),
+                    _makeCategoryContainer('Popular', 5),
+                    _makeCategoryContainer('title3', 6),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50.0),
+              Container(
+                height: 400,
+                child: SingleChildScrollView(
+                  child: StaggeredGrid.count(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    children: const [
+                      StaggeredGridTile.count(
+                        crossAxisCellCount: 2,
+                        mainAxisCellCount: 2,
+                        child: Tile(index: 0),
+                      ),
+                      StaggeredGridTile.count(
+                        crossAxisCellCount: 2,
+                        mainAxisCellCount: 1,
+                        child: Tile(index: 1),
+                      ),
+                      StaggeredGridTile.count(
+                        crossAxisCellCount: 1,
+                        mainAxisCellCount: 1,
+                        child: Tile(index: 2),
+                      ),
+                      StaggeredGridTile.count(
+                        crossAxisCellCount: 1,
+                        mainAxisCellCount: 1,
+                        child: Tile(index: 3),
+                      ),
+                      StaggeredGridTile.count(
+                        crossAxisCellCount: 4,
+                        mainAxisCellCount: 2,
+                        child: Tile(index: 4),
+                      ),
+                      StaggeredGridTile.count(
+                        crossAxisCellCount: 2,
+                        mainAxisCellCount: 2,
+                        child: Tile(index: 5),
+                      ),
+                      StaggeredGridTile.count(
+                        crossAxisCellCount: 2,
+                        mainAxisCellCount: 2,
+                        child: Tile(index: 6),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// make category container widget.
+  Widget _makeCategoryContainer(String title, int id) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          this._activeCategory = id;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 7.5, horizontal: 15.0),
+        margin: EdgeInsets.symmetric(horizontal: 5.0),
+        child: Text(
+          title,
+          style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500, color: (id == _activeCategory) ? Colors.white : Colors.black)
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          color: (id == _activeCategory) ? Colors.grey[700] : Colors.grey[200],
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile({
+    Key? key,
+    required this.index,
+    this.extent,
+    this.backgroundColor,
+    this.bottomSpace,
+  }) : super(key: key);
+
+  final int index;
+  final double? extent;
+  final double? bottomSpace;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final child = Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        color: Color(0xFFEAEAEA),
+      ),
+      height: extent,
+      child: Center(
+        child: CircleAvatar(
+          minRadius: 20,
+          maxRadius: 20,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          child: Text('$index', style: const TextStyle(fontSize: 20)),
+        ),
+      ),
+    );
+
+    if (bottomSpace == null) {
+      return child;
+    }
+
+    return Column(
+      children: [
+        Expanded(child: child),
+        Container(
+          height: bottomSpace,
+          color: Colors.green,
+        )
+      ],
+    );
   }
 }
